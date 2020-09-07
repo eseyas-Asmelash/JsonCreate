f324b11f1b63ede9493fa39f9094878c77763c23
eseyas.asmelash <ezaweld@gmail.com>
2020-09-07 10:30:13 +0200

    Add project files.

---
+++ JsonCreate/Enployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonCreate
+{
+    class Enployee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeId { get; set; }
+        public string Designation { get; set; }
+    }
+}
---
+++ JsonCreate/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace JsonCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enployee employee = new Enployee {
+            FirstName ="Sam",
+            LastName ="Connon",
+            EmployeeId = 123456,
+            Designation = "bed tester"
+            };
+            
+
+            string json = JsonConvert.SerializeObject(employee);
+            string path = @"C:\Users\aies2\Desktop\employee.json";
+
+            if(File.Exists(path))
+            {
+                File.Delete(path);
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine(json.ToString());
+                    writer.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine(json.ToString());
+                    writer.Close();
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
